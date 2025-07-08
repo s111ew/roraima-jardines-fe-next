778c49d22a8a8251fc367427024fcb1d1f36a5dd
s111ew <167631312+s111ew@users.noreply.github.com>
2025-07-08 14:15:02 +0100

    Updates

---
+++ data/text/hero.js
@@ -0,0 +1,21 @@
+const heroText = {
+  title: "Sustrato y abono 100% ecológico",
+  subtitle: "Para plantas más sanas y frutos y vegetales más sabrosos",
+  buttonText: "Encuentra un punto de venta",
+  information: [
+    {
+      title: "100% Ecológico",
+      subtitle: "Evita la dependencia de abonos de origen químico.",
+    },
+    {
+      title: "Ahorra agua",
+      subtitle: "Gracias a su alto contenido en materia orgánica.",
+    },
+    {
+      title: "Hecho en Aragón",
+      subtitle: "Producico y envasado en Pinseque, Zaragoza.",
+    },
+  ],
+};
+
+export default heroText;
