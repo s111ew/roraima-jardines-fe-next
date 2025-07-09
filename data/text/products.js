c563a9d3a6a9cf26d0b987c88680744ebfa2535c
s111ew <167631312+s111ew@users.noreply.github.com>
2025-07-09 15:24:13 +0100

    Updates

--- data/text/products.js
+++ data/text/products.js
@@ -11,12 +11,12 @@ const productsText = {
         { key: 4, src: `${prefix}/product_showcase/Sustrato4.webp` },
       ],
       title: "Sustrato Universal",
-      desc: "Nuestro sustrato contiene todos los nutrientes necesarios para que tus plantas en jardín o maceta puedan desarrollarse a plenitud y las disfrutes en su máximo esplendor.",
+      desc: "Nuestro sustrato contiene todos los nutrientes necesarios de liberación lenta para que tus plantas en jardín o maceta puedan desarrollarse a plenitud y las disfrutes en su máximo esplendor.",
       sizes: "5l, 10l, 20l, 50l",
       accordionTitlePrimary: "Propiedades Físicas",
       accordionContentPrimary: [
         "100% ecológico ya que es de origen orgánico",
-        "Aporta los nutrientes necesarios para el desarrollo de plantas, arbustos, árboles, césped y gramíneas",
+        "Aporta los nutrientes necesarios de liberación lenta para el desarrollo de plantas, arbustos, árboles, césped y gramíneas",
         "Ahorro de agua, por su estructura permite la retención de humedad y a la vez da salida al exceso de agua",
         "Aumenta la materia orgánica en el suelo. Reduce el uso de abonos de origen químico",
         "Mejora la permeabilidad del suelo",
@@ -41,7 +41,7 @@ const productsText = {
       accordionTitlePrimary: "Propiedades Físicas",
       accordionContentPrimary: [
         "100% ecológico ya que es de origen orgánico",
-        "Aporta los nutrientes necesarios para el desarrollo de plantas, arbustos, árboles, césped y gramíneas",
+        "Aporta los nutrientes necesarios de liberación lenta para el desarrollo de plantas, arbustos, árboles, césped y gramíneas",
         "Reduce las oscilaciones del PH del suelo.",
         "Favorece la aireación y oxigenación.",
         "Aumenta la retención de agua.",
---
+++ data/text/puntosDeVenta.js
@@ -0,0 +1,11 @@
+import testimonialText from "./testimonials";
+
+const puntosText = {
+  title: "Puntos De Venta",
+  subtitles: [
+    "Estamos en venta exclusivamente en los siguientes pequeños comercios y viveros.",
+    "Introduce tu código postal en el buscador para encontrar tu tienda más próxima.",
+  ],
+};
+
+export default puntosText;
